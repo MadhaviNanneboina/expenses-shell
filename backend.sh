271ec1cbc6cbe284f09975d57ad8679b70d89026
unknown <madhusagardasari@gmail.com>
2024-04-23 14:52:33 +0800

    backend config5

--- backend.sh
+++ backend.sh
@@ -25,10 +25,10 @@ fi
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-    echo -e "$2 .....$R FAILURE $N"
+    echo "$2 .....FAILURE"
     exit 1
 else
-    echo -e "$2 .....$G SUCCESS $N"
+    echo "$2 .....SUCCESS"
 fi
 }
 
