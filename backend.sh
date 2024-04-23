f18e8e7a7fd814db0924caca788d0c2856444c9e
unknown <madhusagardasari@gmail.com>
2024-04-23 15:42:04 +0800

    backend config5

--- backend.sh
+++ backend.sh
@@ -56,3 +56,18 @@ else
     echo -e "user already exists...$Y SKIPPING $N"
 
 fi
+mkdir -p /app
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "downloading backend code"
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "unzipping the backend code"
+
+npm install
+VALIDATE $? "installing dependencies"
+
+
+
